45b375743e548661b04b49282a1d72ba6c285ce9
Martin Blackburn <walking_fishy@hotmail.com>
2013-06-19 16:45:11 +0100

    can now add and remove rows

--- js/wireframe.js
+++ js/wireframe.js
@@ -11,14 +11,47 @@ https://github.com/MartinBlackburn/wireframe
 
 Wireframe = function() 
 {   
+    var body = $("body");
+    var addRowButton = $(".addRowButton");
+    var removeRowButton = $(".removeRowButton");
+    
     //add a new row at the end
-    function addRow() {
+    addRowButton.click(function(event) {
+        event.preventDefault()
         
+        addRow();
+        
+        bindControls();
+    });
+    
+    function addRow() {
+        console.log("adding row");
+        
+        //row and content elements
+        var row = $("<div class='row'></div>");
+        var content = $("<div class='content'></div>");
+        
+        //add content to the row
+        row.prepend(content);
+        
+        //add row to the bottom
+        body.append(row);
+        
+        //selected the new row
+        selectElement(row);
     }
     
     //remove a row, and anything within it
-    function removeRow() {
+    removeRowButton.click(function(event) {
+        event.preventDefault()
         
+        removeRow();
+        
+        bindControls();
+    });
+    
+    function removeRow() {
+        $(".row.selected").remove();
     }
     
     //add column, based on size given
@@ -69,6 +102,42 @@ Wireframe = function()
     //change background of current item (row or column)
     function changeBackground(element) {
         $(element).toggleClass("altBG");
+    }
+    
+    //make all columns the same height within a row
+    function equaliseColumns() {
+        
+    }
+    
+    //update all controls
+    function bindControls() {
+        //unbind all current controls
+        $('.row, .column-[class*="column-"]').unbind();
+        
+        //add all new controls
+        $('.row, .column-[class*="column-"]').click(function(event) {
+            event.preventDefault()
+            
+            selectElement($(this));
+        });
+    }
+    
+    //select an element, update main controls with new options
+    function selectElement(element) {
+        //unselect all elements
+        $(".selected").removeClass("selected");
+        
+        //add selected class
+        $(element).addClass("selected");
+        
+        updateMainControls();
+    }
+    
+    //update main controls, with new controls, depending what is selected
+    function updateMainControls() {
+        //if row selected
+        
+        //if column selected
     }
     
     //save to url
