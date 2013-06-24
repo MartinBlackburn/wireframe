c12d78ff336969f063888cd087f50b6747477afa
Martin Blackburn <walking_fishy@hotmail.com>
2013-06-24 11:39:42 +0100

    basic url saving (rows and columns), updated respond css

--- js/wireframe.js
+++ js/wireframe.js
@@ -77,15 +77,18 @@ Wireframe = function()
                 break;
             case "center":
                 //center columns with selected row
-                $(".selected .content").toggleClass("centered");
+                $(".selected").toggleClass("centered");
+                save();
                 break;
             case "remove":
                 //remove selected element
                 $(".selected").remove();
+                save();
                 break;
             case "toggleBG":
                 //change background color of selected
                 $(".selected").toggleClass("altBG");
+                save();
                 break;
         }
     });
@@ -151,6 +154,8 @@ Wireframe = function()
             if(element == "row") {
                 $(".row").last().addClass("selected");
             }
+            
+            save();
         });
     }
     
@@ -201,7 +206,89 @@ Wireframe = function()
     //save to url
     function save()
     {
+        var url = "";
         
+        //add rows to url
+        $(".row").each(function()
+        {
+            var row = $(this);
+            
+            url += "r";
+                        
+            if(row.hasClass("altBG"))
+            {
+                url += "-alt";
+            }
+            if(row.hasClass("centered"))
+            {
+                url += "-c";
+            }
+            
+            url += ":";
+            
+            //add columns to url
+            row.find("[class*='column-']").each(function()
+            {
+                var column = $(this);
+                
+                url += "c-";
+                
+                if(column.hasClass("column-1"))
+                {
+                    url += "1";
+                }
+                if(column.hasClass("column-2"))
+                {
+                    url += "2";
+                }
+                if(column.hasClass("column-3"))
+                {
+                    url += "3";
+                }
+                if(column.hasClass("column-4"))
+                {
+                    url += "4";
+                }
+                if(column.hasClass("column-5"))
+                {
+                    url += "5";
+                }
+                if(column.hasClass("column-6"))
+                {
+                    url += "6";
+                }
+                if(column.hasClass("column-7"))
+                {
+                    url += "7";
+                }
+                if(column.hasClass("column-8"))
+                {
+                    url += "8";
+                }
+                if(column.hasClass("column-9"))
+                {
+                    url += "9";
+                }
+                if(column.hasClass("column-10"))
+                {
+                    url += "10";
+                }
+                if(column.hasClass("column-11"))
+                {
+                    url += "11";
+                }
+                if(column.hasClass("column-12"))
+                {
+                    url += "12";
+                }
+                
+                url += ":";
+            });
+            
+            url += "|";
+        });
+        
+        console.log(url);
     }
     
     //load from url
